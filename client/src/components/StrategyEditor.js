7c430ad2c3acae9fbc96c9cbfa0117dfe85b0f21
JunHao247 <onlyjunhao247@gmail.com>
2024-07-17 17:15:39 +0900

    Update graph plot and enhance UI padding

--- client/src/components/StrategyEditor.js
+++ client/src/components/StrategyEditor.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const StrategyEditor = ({ onStrategyChange, presetStrategy }) => (
-  <textarea class="strategy"
-    onChange={(e) => onStrategyChange(e.target.value)} 
-    placeholder="Enter your strategy here" 
-    rows="20" 
-    cols="50"
-    defaultValue={presetStrategy} // Set the default value to the preset strategy
-    style={{ width: '100%', padding: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
+  <textarea
+    className="strategy"
+    onChange={(e) => onStrategyChange(e.target.value)}
+    defaultValue={presetStrategy}
+    placeholder='Enter your strategy here'
   />
 );
 
